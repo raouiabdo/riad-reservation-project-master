07c86b1488c3ebbc14bf765ac4e7e8a7faf2a7e8
RaouiAbdelkamel <Abdelkamel.Raoui@emsi-edu.ma>
2024-12-13 00:18:36 +0100

    test from raoui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    {{-- test from raoui --}}
     <div class="container text-center mt-5">
         <h1 class="display-4">Hello, bienvenue sur notre site!</h1>
 
